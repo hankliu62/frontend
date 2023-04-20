7ad65a03c679c8fcf4fe52a883a50879803fdf66
hankliu62 <397694072@qq.com>
2023-04-20 21:23:15 +0800

    feat(component): modify header use info

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,8 +22,8 @@ import { useRouter } from "next/router";
 import React, { Fragment, useState } from "react";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
+  name: "Hank Liu",
+  email: "hankliu62@163.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
