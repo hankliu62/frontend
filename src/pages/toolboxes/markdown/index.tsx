85e6180cba7a2073cf2eab30ed87ddb583143c55
hankliu62 <397694072@qq.com>
2023-05-05 09:34:00 +0800

    feat(toolboxes): add markdown page annotation

--- src/pages/toolboxes/markdown/index.tsx
+++ src/pages/toolboxes/markdown/index.tsx
@@ -50,6 +50,11 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 `;
 
+/**
+ * 在线Markdown编辑器网站
+ *
+ * @returns
+ */
 export default function MarkdownPage() {
   const [content, setContent] = useState<string>();
 
