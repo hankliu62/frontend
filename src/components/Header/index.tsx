e94e98bc453e3fc069173a24f2c4a819ae68b87b
hankliu <397694072@qq.com>
2024-05-09 09:26:35 +0800

    feat(frontend): refine header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,8 @@
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import {
-  AdjustmentsHorizontalIcon,
   Bars3Icon,
   BellIcon,
-  CircleStackIcon,
-  CodeBracketSquareIcon,
-  CommandLineIcon,
-  DocumentTextIcon,
-  HashtagIcon,
-  IdentificationIcon,
-  PlayCircleIcon,
-  QrCodeIcon,
-  VariableIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { Popover, Tooltip } from "antd";
@@ -144,7 +134,7 @@ export default function Header({ className }: IHeaderProps) {
                             key={item.name}
                             content={
                               <div className="relative grid grid-cols-2 gap-4 bg-white px-2 py-3 sm:gap-6 sm:p-4 2xl:grid-cols-3">
-                                {item.children.map((subItem) => {
+                                {(item as any)?.children?.map((subItem) => {
                                   const subLink = (
                                     <a
                                       key={subItem.name}
@@ -256,14 +246,14 @@ export default function Header({ className }: IHeaderProps) {
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
-                      <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <MenuButton className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
                           src={user.imageUrl}
                           alt=""
                         />
-                      </Menu.Button>
+                      </MenuButton>
                     </div>
                     <Transition
                       as={Fragment}
@@ -274,45 +264,45 @@ export default function Header({ className }: IHeaderProps) {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
+                          <MenuItem key={item.name}>
+                            {({ focus }) => (
                               <a
                                 href={`${getRoutePrefix()}${item.href}`}
                                 className={classNames(
-                                  active ? "bg-gray-100" : "",
+                                  focus ? "bg-gray-100" : "",
                                   "block px-4 py-2 text-sm text-gray-700"
                                 )}
                               >
                                 {item.name}
                               </a>
                             )}
-                          </Menu.Item>
+                          </MenuItem>
                         ))}
-                      </Menu.Items>
+                      </MenuItems>
                     </Transition>
                   </Menu>
                 </div>
               </div>
               <div className="-mr-2 flex md:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-purple-800 p-2 text-purple-400 hover:bg-purple-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-purple-800">
+                <DisclosureButton className="inline-flex items-center justify-center rounded-md bg-purple-800 p-2 text-purple-400 hover:bg-purple-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-purple-800">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </DisclosureButton>
               </div>
             </div>
           </div>
 
-          <Disclosure.Panel className="border-b border-purple-700 bg-white md:hidden">
+          <DisclosurePanel className="border-b border-purple-700 bg-white md:hidden">
             <div className="space-y-1 px-2 py-3 sm:px-3">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <DisclosureButton
                   key={item.name}
                   as="a"
                   href={item.href}
@@ -337,7 +327,7 @@ export default function Header({ className }: IHeaderProps) {
                   }
                 >
                   {item.name}
-                </Disclosure.Button>
+                </DisclosureButton>
               ))}
             </div>
             <div className="border-t border-purple-700 pb-3 pt-4">
@@ -367,18 +357,18 @@ export default function Header({ className }: IHeaderProps) {
               </div>
               <div className="mt-3 space-y-1 px-2">
                 {userNavigation.map((item) => (
-                  <Disclosure.Button
+                  <DisclosureButton
                     key={item.name}
                     as="a"
                     href={item.href}
                     className="block rounded-md px-3 py-2 text-base font-medium text-purple-400 hover:bg-purple-700 hover:text-white"
                   >
                     {item.name}
-                  </Disclosure.Button>
+                  </DisclosureButton>
                 ))}
               </div>
             </div>
-          </Disclosure.Panel>
+          </DisclosurePanel>
         </>
       )}
     </Disclosure>
