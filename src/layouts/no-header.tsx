c705b2be32100b24ed2d6e63cbdcf7888448e6a0
hankliu <397694072@qq.com>
2024-05-09 08:33:49 +0800

    feat(frontend): refine layouts

--- src/layouts/no-header.tsx
+++ src/layouts/no-header.tsx
@@ -1,35 +1,20 @@
-import dynamic from "next/dynamic";
 import { ReactNode } from "react";
-
-const Footer = dynamic(() => import("@hankliu/rc-footer"), {
-  ssr: false,
-});
-
-const Header = dynamic(() => import("@/components/Header"), {
-  ssr: false,
-});
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-export default function DefaultLayout({ children }: LayoutProps) {
+export default function NoHeaderLayout({ children }: LayoutProps) {
   return (
     <>
       <div
         className="flex h-full min-h-[100vh] w-full flex-col"
         style={{ flexDirection: "column", minHeight: "100vh" }}
       >
-        {/* header */}
-        <Header />
-
         {/* Main content */}
         <main className="flex flex-1 grow-[1] flex-col" style={{ flex: 1 }}>
           {children}
         </main>
-
-        {/* footer */}
-        <Footer />
       </div>
     </>
   );
