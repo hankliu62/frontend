44d245197f7937a018afa75b40c7c9a7620a47a7
hankliu <397694072@qq.com>
2024-05-09 08:25:19 +0800

    feat(frontend): add toolkits pages

---
+++ src/pages/toolkits/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ICss() {
+  return (
+    <div className="flex h-[100vh] w-full">
+      <iframe
+        title="小工具集合"
+        src="https://hankliu62.github.io/toolkits"
+        width="100%"
+        className="border-0"
+      />
+    </div>
+  );
+}
---
+++ src/pages/toolkits/video-player/index.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { PlaySquareOutlined } from "@ant-design/icons";
+import { Button, Input, message } from "antd";
+import { useCallback, useRef, useState } from "react";
+
+import VideoPlayer, {
+  IVideoPlayerImperativeHandles,
+} from "@/components/VideoPlayer";
+
+const TextArea = Input.TextArea;
+
+/**
+ * 在线视频播放网站
+ *
+ * @returns
+ */
+export default function VideoPlayerPage() {
+  const [source, setSource] = useState<string>();
+
+  // 视频播放器
+  const videoPlayer = useRef<IVideoPlayerImperativeHandles>();
+
+  /**
+   * 播放
+   */
+  const onPlay = useCallback(() => {
+    if (!source) {
+      message.error("请输入视频数据源");
+      return;
+    }
+
+    videoPlayer.current.play();
+  }, [source]);
+
+  return (
+    <div className="bg-white p-6">
+      <div className="flex flex-col space-y-6">
+        <div>
+          <label className="text-base font-normal">视频数据源: </label>
+          <div className="mt-2">
+            <TextArea
+              className="w-full"
+              autoSize={{ minRows: 3, maxRows: 20 }}
+              value={source}
+              onChange={(e) => setSource(e.target.value)}
+              placeholder="请输入视频数据源，支持m3u8、flv、rtmp、RTS等格式的视频"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-between pb-4">
+            <label className="text-base font-normal">案例展示: </label>
+
+            <div className="flex items-center space-x-5">
+              <Button
+                className="!inline-flex items-center"
+                icon={<PlaySquareOutlined rev={undefined} />}
+                onClick={onPlay}
+              >
+                立即播放
+              </Button>
+            </div>
+          </div>
+
+          <div>
+            <VideoPlayer
+              className="h-[800px]"
+              ref={videoPlayer}
+              source={source}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
