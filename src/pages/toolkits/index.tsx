9eedad7c6f66132d2ba8ad843088364f4d4e5eb5
hankliu <397694072@qq.com>
2024-05-11 08:20:05 +0800

    feat(frontend): refine toolkits page

--- src/pages/toolkits/index.tsx
+++ src/pages/toolkits/index.tsx
@@ -2,12 +2,16 @@ import { ReactElement } from "react";
 
 import OnlyHeaderLayout from "@/layouts/only-header";
 
+/**
+ * 小工具页面
+ * @returns
+ */
 function ToolKitsPage() {
   return (
     <div className="flex h-[100vh] w-full">
       <iframe
         title="小工具集合"
-        src="https://hankliu62.github.io/toolkits?with-header=1"
+        src="https://hankliu62.github.io/toolkits?with-footer=1"
         width="100%"
         className="border-0"
       />
@@ -20,4 +24,4 @@ ToolKitsPage.getLayout = function getLayout(page: ReactElement) {
   return <OnlyHeaderLayout>{page}</OnlyHeaderLayout>;
 };
 
-export default ToolKitsPage
+export default ToolKitsPage;
